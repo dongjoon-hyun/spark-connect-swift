9edc3101ed177e0e4b88a83d358373e9823b2b12
Dongjoon Hyun <dongjoon@apache.org>
2025-03-10 20:21:26 -0700

    [SPARK-51461] Setup `SparkConnect` Swift package structure and CI to test `build`
    ### What changes were proposed in this pull request?
    
    This PR aims to setup `SparkConnect` Swift package structure and CI to test `build`.
    
    Note that this is a subset of the initial implementation.
    - https://github.com/apache/spark-connect-swift/pull/1
    
    ### Why are the changes needed?
    
    To setup the initial package structure with CI build test coverage before adding the actual code. Currently, the following two OSs are tested.
    - MacOS 15
    - Ubuntu 24.04
    
    According to the standard `Swift` package structure,
    - https://www.swift.org/getting-started/library-swiftpm/#bootstrapping
    
    this PR adds the following structure for `SparkConnect` package. `SparkConnectError.swift` and `BuilderTests.swift` is added in order to fill the empty directories.
    ```
    $ tree .
    .
    ├── dev
    │   └── merge_spark_pr.py
    ├── LICENSE
    ├── Package.swift
    ├── README.md
    ├── Sources
    │   └── SparkConnect
    │       └── SparkConnectError.swift
    └── Tests
        └── SparkConnectTests
            └── BuilderTests.swift
    ```
    
    ### Does this PR introduce _any_ user-facing change?
    
    No. This is not released yet.
    
    ### How was this patch tested?
    
    Pass the CI.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #4 from dongjoon-hyun/SPARK-51461.
    
    Authored-by: Dongjoon Hyun <dongjoon@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

---
+++ Package.swift
@@ -0,0 +1,59 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+import PackageDescription
+
+let package = Package(
+  name: "SparkConnect",
+  platforms: [
+    .macOS(.v15),
+    .iOS(.v18),
+    .watchOS(.v11),
+    .tvOS(.v18),
+  ],
+  products: [
+    .library(
+      name: "SparkConnect",
+      targets: ["SparkConnect"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/grpc/grpc-swift.git", from: "2.1.0"),
+    .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "1.1.0"),
+    .package(url: "https://github.com/grpc/grpc-swift-nio-transport.git", from: "1.0.1"),
+    .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", from: "0.9.0"),
+    .package(url: "https://github.com/google/flatbuffers.git", branch: "v24.3.7"),
+  ],
+  targets: [
+    .target(
+      name: "SparkConnect",
+      dependencies: [
+        .product(name: "GRPCCore", package: "grpc-swift"),
+        .product(name: "GRPCProtobuf", package: "grpc-swift-protobuf"),
+        .product(name: "GRPCNIOTransportHTTP2", package: "grpc-swift-nio-transport"),
+        .product(name: "SwiftyTextTable", package: "SwiftyTextTable"),
+        .product(name: "FlatBuffers", package: "flatbuffers"),
+      ]
+    ),
+    .testTarget(
+      name: "SparkConnectTests",
+      dependencies: ["SparkConnect"]
+    ),
+  ]
+)
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Apache Spark Connect Client for Swift language
+# Apache Spark Connect Client for Swift
+
+[![GitHub Actions Build](https://github.com/apache/spark-connect-swift/actions/workflows/build_and_test.yml/badge.svg)](https://github.com/apache/spark-connect-swift/blob/main/.github/workflows/build_and_test.yml)
+
+This is an experimental Swift library to show how to connect to a remote Apache Spark Connect Server and run SQL statements to manipulate remote data.
+
+So far, this library project is tracking the upstream changes like the [Apache Spark](https://spark.apache.org) 4.0.0 RC2 release and [Apache Arrow](https://arrow.apache.org) project's Swift-support.
+
+## Requirement
+- [Apache Spark 4.0.0 RC2 (March 2025)](https://dist.apache.org/repos/dist/dev/spark/v4.0.0-rc2-bin/)
+- [Swift 6.0 (2024)](https://swift.org)
+- [gRPC Swift 2.1 (March 2025)](https://github.com/grpc/grpc-swift/releases/tag/2.1.0)
+- [gRPC Swift Protobuf 1.0 (March 2025)](https://github.com/grpc/grpc-swift-protobuf/releases/tag/1.1.0)
+- [gRPC Swift NIO Transport 1.0 (March 2025)](https://github.com/grpc/grpc-swift-nio-transport/releases/tag/1.0.1)
+- [Apache Arrow Swift](https://github.com/apache/arrow/tree/main/swift)
