e891c9bb07ad2dba5045e05201528f41a44ac335
Dongjoon Hyun <dongjoon@apache.org>
2025-12-19 14:48:23 -0800

    [SPARK-54786] Upgrade `gRPC Swift Protobuf` to 2.1.2
    ### What changes were proposed in this pull request?
    
    This PR aims to upgrade `gRPC Swift Protobuf` to 2.1.2.
    
    ### Why are the changes needed?
    
    `gRPC Swift Protobuf` 2.1.2 is the first release with tested with Swift 6.2 officially. In addition, it requires the latest `grpc-swift-2` dependency (#268).
    - https://github.com/grpc/grpc-swift-protobuf/releases/tag/2.1.2
      - https://github.com/grpc/grpc-swift-protobuf/pull/88
      - https://github.com/grpc/grpc-swift-protobuf/pull/91
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Pass the CIs.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #270 from dongjoon-hyun/SPARK-54786.
    
    Authored-by: Dongjoon Hyun <dongjoon@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/grpc/grpc-swift-2.git", exact: "2.2.1"),
-    .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", exact: "2.1.1"),
+    .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", exact: "2.1.2"),
     .package(url: "https://github.com/grpc/grpc-swift-nio-transport.git", exact: "2.4.0"),
     .package(url: "https://github.com/google/flatbuffers.git", branch: "v25.9.23"),
   ],
